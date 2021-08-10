f105c202b826f96d0f79ad8dcaf400c79a7ec79b
Marcel Fonseca <marcelsf23br@gmail.com>
2021-08-10 17:53:17 +0200

    finoshed with the crud actions

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,11 @@
 class Pet < ApplicationRecord
+  SPECIES = ['dog', 'cat', 'spider', 'rabbit', 'snake', 'gold fish']
+
   validates :name, presence: true
-  validates :species, inclusion: {in: ['dog', 'cat', 'spider', 'rabbit', 'snake', 'gold fish']}
+  validates :species, inclusion: {in: Pet::SPECIES}
+
+  def days_ago
+    days = Date.today - found_on
+    days.to_i
+  end
 end
